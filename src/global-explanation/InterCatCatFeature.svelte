3dc23e0c0fff0d97bd24711d98413291b15bb5f4
Jay Wong <xiao.hk1997@gmail.com>
2021-05-25 16:08:54 -0400

    Modularize color legend drawing function

--- src/global-explanation/InterCatCatFeature.svelte
+++ src/global-explanation/InterCatCatFeature.svelte
@@ -2,6 +2,7 @@
   import * as d3 from 'd3';
   import { round } from '../utils';
   import { config } from '../config';
+  import { drawHorizontalColorLegend } from './draw';
 
   export let featureData = null;
   export let scoreRange = null;
@@ -90,64 +91,7 @@
     return data;
   };
 
-  /**
-   * Create a horizontal color legend.
-   * @param legendGroup
-   * @param legendConfig
-   * @param largestAbs
-   */
-  const drawHorizontalColorLegend = (legendGroup, legendConfig, largestAbs) => {
-    // Define the gradient
-    let legendGradientDef = legendGroup.append('defs')
-      .append('linearGradient')
-      .attr('x1', 0)
-      .attr('y1', 0)
-      .attr('x2', 1)
-      .attr('y2', 0)
-      // TODO: use state to track the number of legend-gradient and make each of
-      // them unique
-      .attr('id', 'legend-gradient');
-
-    legendGradientDef.append('stop')
-      .attr('stop-color', legendConfig.startColor)
-      .attr('offset', 0);
-
-    legendGradientDef.append('stop')
-      .attr('stop-color', '#ffffff')
-      .attr('offset', 0.5);
-
-    legendGradientDef.append('stop')
-      .attr('stop-color', legendConfig.endColor)
-      .attr('offset', 1);
-
-    legendGroup.append('rect')
-      .attr('x', 0)
-      .attr('y', 0)
-      .attr('width', legendConfig.width)
-      .attr('height', legendConfig.height)
-      .style('fill', 'url(#legend-gradient)')
-      .style('stroke', 'hsla(0, 0%, 0%, 0.5)')
-      .style('stroke-width', 0.3);
-
-    // Draw the legend axis
-    let legendScale = d3.scaleLinear()
-      .domain([-largestAbs, largestAbs])
-      .range([0, legendConfig.width]);
-
-    legendGroup.append('g')
-      .attr('transform', `translate(${0}, ${legendConfig.height})`)
-      .call(d3.axisBottom(legendScale).ticks(5));
-
-    legendGroup.append('text')
-      .attr('class', 'legend-title')
-      .attr('x', -10)
-      .attr('y', 0)
-      .style('dominant-baseline', 'hanging')
-      .style('text-anchor', 'end')
-      .text('Score');
-  };
-
-  const drawFeatureBar = (featureData) => {
+  const drawFeature = (featureData) => {
     console.log(featureData);
     let svgSelect = d3.select(svg);
 
@@ -352,10 +296,6 @@
       .style('fill', colors.histAxis);
 
   };
-
-  let drawFeature = null;
-
-  drawFeature = drawFeatureBar;
 
   $: featureData && drawFeature(featureData);
 
--- src/global-explanation/InterContContFeature.svelte
+++ src/global-explanation/InterContContFeature.svelte
@@ -2,6 +2,7 @@
   import * as d3 from 'd3';
   import { round, transpose2dArray } from '../utils';
   import { config } from '../config';
+  import { drawHorizontalColorLegend } from './draw';
 
   export let featureData = null;
   export let scoreRange = null;
@@ -10,9 +11,6 @@
   let svg = null;
 
   // Visualization constants
-  // const svgPadding = {
-  //   top: 30, right: 10, bottom: 30, left: 25
-  // };
   const densityHeight = 90;
 
   // Viewbox width and height
@@ -33,64 +31,6 @@
     hist: 'hsl(222, 10%, 93%)',
     histAxis: 'hsl(222, 10%, 70%)',
     line0: 'hsla(222, 0%, 0%, 5%)'
-  };
-
-  /**
-   * Create a horizontal color legend.
-   * @param legendGroup
-   * @param legendConfig
-   * @param largestAbs
-   */
-  const drawHorizontalColorLegend = (legendGroup, legendConfig, largestAbs) => {
-    // Define the gradient
-    let legendGradientDef = legendGroup.append('defs')
-      .append('linearGradient')
-      .attr('x1', 0)
-      .attr('y1', 0)
-      .attr('x2', 1)
-      .attr('y2', 0)
-      // TODO: use state to track the number of legend-gradient and make each of
-      // them unique
-      .attr('id', 'legend-gradient');
-
-    legendGradientDef.append('stop')
-      .attr('stop-color', legendConfig.startColor)
-      .attr('offset', 0);
-
-    legendGradientDef.append('stop')
-      .attr('stop-color', '#ffffff')
-      .attr('offset', 0.5);
-
-    legendGradientDef.append('stop')
-      .attr('stop-color', legendConfig.endColor)
-      .attr('offset', 1);
-
-    legendGroup.append('rect')
-      .attr('x', 0)
-      .attr('y', 0)
-      .attr('width', legendConfig.width)
-      .attr('height', legendConfig.height)
-      .style('fill', 'url(#legend-gradient)')
-      .style('stroke', 'hsla(0, 0%, 0%, 0.5)')
-      .style('stroke-width', 0.3);
-
-    // Draw the legend axis
-    let legendScale = d3.scaleLinear()
-      .domain([-largestAbs, largestAbs])
-      .range([0, legendConfig.width])
-      .nice();
-
-    legendGroup.append('g')
-      .attr('transform', `translate(${0}, ${legendConfig.height})`)
-      .call(d3.axisBottom(legendScale).ticks(5));
-
-    legendGroup.append('text')
-      .attr('class', 'legend-title')
-      .attr('x', -10)
-      .attr('y', 0)
-      .style('dominant-baseline', 'hanging')
-      .style('text-anchor', 'end')
-      .text('Score');
   };
 
   const drawFeatureBar = (featureData) => {
