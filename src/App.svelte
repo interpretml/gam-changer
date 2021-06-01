b21861870981cc4be94c1ba0949139890a4dae72
Jay Wong <xiao.hk1997@gmail.com>
2021-06-01 11:14:39 -0400

    Move some html css to app component

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,20 @@
 </script>
 
 <style type='text/scss'>
+	.stand-alone-page {
+		color: #333;
+		box-sizing: border-box;
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+		font-size: 16px;
+	}
 </style>
+
+<!-- Import Google font and font awesome -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link
+		href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap"
+		rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
 
 <div class='stand-alone-page'>
 	<Main />
