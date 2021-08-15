5911c05325e8c789ea2d69c862592ef2559f5a5b
Jay Wang <jay@zijie.wang>
2021-08-15 18:16:13 -0400

    Ditch .wasm files and use base64 instead
    Signed-off-by: Jay Wang <jay@zijie.wang>

--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,7 @@ import preprocess from 'svelte-preprocess';
 import inlineSvg from 'rollup-plugin-inline-svg';
 
 const production = !process.env.ROLLUP_WATCH;
+const jupyter = !process.env.JUPYTER;
 
 function serve() {
   let server;
