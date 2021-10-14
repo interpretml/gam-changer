96f85d1bb1501fd5073b1e1461fdb7c45a4a5abb
Jay Wang <jay@zijie.wang>
2021-10-14 00:53:07 -0400

    Fix historical recovery bug on categorical features
    Signed-off-by: Jay Wang <jay@zijie.wang>

--- publish.sh
+++ publish.sh
@@ -2,9 +2,13 @@
 
 npm run build
 
-if [ $# -eq 1 ]
-then
-  surge public https://xiaohk-gam-changer-$1.surge.sh
-else
-  surge public https://xiaohk-gam-changer.surge.sh
-fi
+# if [ $# -eq 1 ]
+# then
+#   surge public https://xiaohk-gam-changer-$1.surge.sh
+# else
+#   surge public https://xiaohk-gam-changer.surge.sh
+# fi
+
+cd public
+vercel
+cd ..
--- src/utils/ebm-edit.js
+++ src/utils/ebm-edit.js
@@ -1,5 +1,5 @@
 /**
- * Overwrite the edge definition in the EBM WASM model.
+ * Infer new bin edges and scores from the node data.
  * @param {object} curNodeData Node data in `state`
  */
 export const getBinEdgeScore = (curNodeData) => {
@@ -8,27 +8,42 @@ export const getBinEdgeScore = (curNodeData) => {
   let newBinEdges = [];
   let newScores = [];
 
-  // The left point will always have index 0
-  let curPoint = curNodeData[0];
-  let curEBMID = 0;
+  if (curNodeData[0] === undefined) {
+    // Categorical variable
+    for (let i = 1; i < Object.keys(curNodeData).length + 1; i++) {
+      newBinEdges.push(curNodeData[i].id);
+      newScores.push(curNodeData[i].y);
+    }
 
-  while (curPoint.rightPointID !== null) {
-    // Collect x and y
+    return { newBinEdges: newBinEdges, newScores: newScores };
+
+  } else {
+    // Continuous variable
+    // The left point will always have index 0
+    let curPoint = curNodeData[0];
+    let curEBMID = 0;
+
+    // Continuous and categorical features require different ways to reconstruct
+    // the bin definitions
+    while (curPoint.rightPointID !== null) {
+      // Collect x and y
+      newBinEdges.push(curPoint.x);
+      newScores.push(curPoint.y);
+
+      // Update the new ID so we can map them to bin indexes later (needed for
+      // selection to check sample number)
+      curPoint.ebmID = curEBMID;
+      curEBMID++;
+
+      curPoint = curNodeData[curPoint.rightPointID];
+    }
+
+    // Add the right node
     newBinEdges.push(curPoint.x);
     newScores.push(curPoint.y);
-
-    // Update the new ID so we can map them to bin indexes later (needed for
-    // selection to check sample number)
     curPoint.ebmID = curEBMID;
-    curEBMID++;
 
-    curPoint = curNodeData[curPoint.rightPointID];
+    return { newBinEdges: newBinEdges, newScores: newScores };
   }
 
-  // Add the right node
-  newBinEdges.push(curPoint.x);
-  newScores.push(curPoint.y);
-  curPoint.ebmID = curEBMID;
-
-  return {newBinEdges: newBinEdges, newScores: newScores};
 };
