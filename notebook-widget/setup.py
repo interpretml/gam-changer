b204668e3b8d991f0b505a6b353b3e44dd00fe8d
Jay Wang <jay@zijie.wang>
2022-05-29 16:50:49 -0700

    Bump version: 0.1.3 → 0.1.4

--- notebook-widget/setup.py
+++ notebook-widget/setup.py
@@ -36,6 +36,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/xiaohk/gam-changer',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
