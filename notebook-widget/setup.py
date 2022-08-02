dbe235e75be5772ede157d34d70a1b958c7a6eb7
Jay Wang <jay@zijie.wang>
2022-08-02 09:19:15 -0700

    Remove dependency on html
    Signed-off-by: Jay Wang <jay@zijie.wang>

--- notebook-widget/setup.py
+++ notebook-widget/setup.py
@@ -4,38 +4,38 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-requirements = ['numpy', 'pandas', 'html', 'ipython']
+requirements = ["numpy", "pandas", "ipython"]
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="Jay Wang",
-    author_email='jayw@zijie.wang',
-    python_requires='>=3.6',
+    author_email="jayw@zijie.wang",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="A Python package to run GAM Changer in your computation notebooks.",
     install_requires=requirements,
     license="MIT license",
     long_description=readme,
     include_package_data=True,
-    keywords='gamchanger',
-    name='gamchanger',
-    packages=find_packages(include=['gamchanger', 'gamchanger.*']),
-    test_suite='tests',
+    keywords="gamchanger",
+    name="gamchanger",
+    packages=find_packages(include=["gamchanger", "gamchanger.*"]),
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/xiaohk/gam-changer',
-    version='0.1.5',
+    url="https://github.com/xiaohk/gam-changer",
+    version="0.1.5",
     zip_safe=False,
 )
